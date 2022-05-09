02f271c26c4290b3d072db0bcea1cdb9f27c49a7
danlaptev <danlaptev@yandex-team.ru>
2022-05-09 14:01:41 +0300

    удалил тесты

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('App 2 header has been rendered', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/App 2/i);
-  expect(linkElement).toBeInTheDocument();
-});
